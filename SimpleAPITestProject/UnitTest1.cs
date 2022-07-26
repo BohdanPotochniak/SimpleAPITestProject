10e9190572dc2b424f7c7a94343ca5c5365737d6
Bohdan Potochniak <bohdan.potochniak@globallogic.com>
2022-07-26 09:53:31 +0300

    Add project files.

---
+++ SimpleAPITestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SimpleAPITestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
