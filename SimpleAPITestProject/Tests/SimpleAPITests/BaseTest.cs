416e919bc8194366d23c2a1129c7072a2242d918
Bohdan Potochniak <bohdan.potochniak@globallogic.com>
2022-08-12 14:54:44 +0300

    add GET API tests

---
+++ SimpleAPITestProject/Tests/SimpleAPITests/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace SimpleAPITestProject.Tests.SimpleAPITests
+{
+    internal class BaseTest
+    {
+        [Test]
+        public static void setURL()
+        {
+            string url = "https://jsonplaceholder.typicode.com";
+            var client = new RestClient(url + "/users/1");
+            var request = new RestRequest();
+            var response = client.Get(request);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ SimpleAPITestProject/Tests/SimpleAPITests/GetOperations.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleAPITestProject.SimpleAPITests
+{
+    public class GetOperations
+    {
+        public void some()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
